27b4f9aa82b5d34d19bcbe7d64b94186916dfb2e
Arunkrishna02 <160992577+Arunkrishna02@users.noreply.github.com>
2026-09-03 06:55:02 +0530

    Implement subtract function with unit tests
    Added subtract function and its tests.

--- GitHub-Actions-Zero-to-Hero-main/src/addition.py
+++ GitHub-Actions-Zero-to-Hero-main/src/addition.py
@@ -1,8 +1,17 @@
 # app.py
 # This is a test commit
+
 def add(a, b):
     return a + b
 
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
+
+def subtract(a, b):
+    return a - b
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+    assert subtract(10, 5) == 5
